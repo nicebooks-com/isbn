9c09b9239e20d6ad0699044c2d05c6fabd99ce96
Benjamin Morel <benjamin.morel@gmail.com>
2016-04-30 13:42:18 +0200

    More readable variable naming in Formatter

--- src/Internal/Formatter.php
+++ src/Internal/Formatter.php
@@ -64,36 +64,36 @@ class Formatter
     public static function getRangeInfo($isbn)
     {
         $length = strlen($isbn);
-        $prefix = ($length === 10) ? '978' : substr($isbn, 0, 3);
-        $digits = ($length === 10) ? $isbn : substr($isbn, 3);
+        $isbnPrefix = ($length === 10) ? '978' : substr($isbn, 0, 3);
+        $isbnDigits = ($length === 10) ? $isbn : substr($isbn, 3);
 
         foreach (self::getRanges() as $rangeData) {
-            list ($rangePrefix, $groupIdentifier, $groupName, $ranges) = $rangeData;
+            list ($eanPrefix, $groupIdentifier, $groupName, $ranges) = $rangeData;
 
-            if ($prefix !== $rangePrefix) {
+            if ($isbnPrefix !== $eanPrefix) {
                 continue;
             }
 
             $groupLength = strlen($groupIdentifier);
-            $isbnGroup = substr($digits, 0, $groupLength);
+            $isbnGroup = substr($isbnDigits, 0, $groupLength);
 
             if ($isbnGroup !== $groupIdentifier) {
                 continue;
             }
 
             $rangeInfo = new IsbnRangeInfo;
-            $rangeInfo->groupIdentifier = ($length === 10 ? $groupIdentifier : $prefix . '-' . $groupIdentifier);
+            $rangeInfo->groupIdentifier = ($length === 10 ? $groupIdentifier : $eanPrefix . '-' . $groupIdentifier);
             $rangeInfo->groupName = $groupName;
 
             foreach ($ranges as $range) {
                 list ($rangeLength, $rangeStart, $rangeEnd) = $range;
-                $rangeValue = substr($digits, $groupLength, $rangeLength);
-                $lastDigits = substr($digits, $groupLength + $rangeLength, -1);
-                $checkDigit = substr($digits, -1);
+                $rangeValue = substr($isbnDigits, $groupLength, $rangeLength);
+                $lastDigits = substr($isbnDigits, $groupLength + $rangeLength, -1);
+                $checkDigit = substr($isbnDigits, -1);
 
                 if (strcmp($rangeValue, $rangeStart) >= 0 && strcmp($rangeValue, $rangeEnd) <= 0) {
                     if ($length === 13) {
-                        $rangeInfo->parts = array($prefix, $isbnGroup, $rangeValue, $lastDigits, $checkDigit);
+                        $rangeInfo->parts = array($isbnPrefix, $isbnGroup, $rangeValue, $lastDigits, $checkDigit);
                     } else {
                         $rangeInfo->parts = array($isbnGroup, $rangeValue, $lastDigits, $checkDigit);
                     }
