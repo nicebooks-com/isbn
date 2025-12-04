7023712620dd7c15d5679060a228c9e97174bcdd
Benjamin Morel <benjamin.morel@gmail.com>
2025-12-04 14:48:54 +0100

    Add #[Override] attributes

--- src/Isbn10.php
+++ src/Isbn10.php
@@ -4,33 +4,41 @@ declare(strict_types=1);
 
 namespace Nicebooks\Isbn;
 
+use Override;
+
 final readonly class Isbn10 extends Isbn
 {
+    #[Override]
     public static function of(string $isbn) : Isbn10
     {
         return parent::of($isbn)->to10();
     }
 
+    #[Override]
     public function is10() : bool
     {
         return true;
     }
 
+    #[Override]
     public function is13() : bool
     {
         return false;
     }
 
+    #[Override]
     public function isConvertibleTo10() : bool
     {
         return true;
     }
 
+    #[Override]
     public function to10() : Isbn10
     {
         return $this;
     }
 
+    #[Override]
     public function to13() : Isbn13
     {
         return self::newIsbn13(Internal\Converter::convertIsbn10to13($this->isbn));
--- src/Isbn13.php
+++ src/Isbn13.php
@@ -4,33 +4,41 @@ declare(strict_types=1);
 
 namespace Nicebooks\Isbn;
 
+use Override;
+
 final readonly class Isbn13 extends Isbn
 {
+    #[Override]
     public static function of(string $isbn) : Isbn13
     {
         return parent::of($isbn)->to13();
     }
 
+    #[Override]
     public function is10() : bool
     {
         return false;
     }
 
+    #[Override]
     public function is13() : bool
     {
         return true;
     }
 
+    #[Override]
     public function isConvertibleTo10() : bool
     {
         return str_starts_with($this->isbn, '978');
     }
 
+    #[Override]
     public function to10() : Isbn10
     {
         return self::newIsbn10(Internal\Converter::convertIsbn13to10($this->isbn));
     }
 
+    #[Override]
     public function to13() : Isbn13
     {
         return $this;
